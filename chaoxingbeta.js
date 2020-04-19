ac6f52d0af66b7bd68e099f35b0f36e474877b5a
LDS-Skeleton <50533276+LDS-Skeleton@users.noreply.github.com>
2020-04-19 11:03:29 +0800

    Update chaoxingbeta.js

--- chaoxingbeta.js
+++ chaoxingbeta.js
@@ -33,7 +33,7 @@ $("body").append("<div id='skdiv'></div>");
 $("#skdiv").html("<p ><span style='font-weight:bold;    font-size: large;'>超星刷课脚本</span>（可用鼠标拖动）<p><p>最后更新时间：2020/4/46/16:42<br/>更新内容:支持一个章节多个视频的观看</p><div id='content' style='   border-top: 2px solid;'></div>");
 $('#content').html('<div ><p id="rate_txt" >播放速度：1X</p><div style="float:left"><button id="b1">▲</button><button id="b2">▼</button></div></div><button id="startplay" onclick="init()">点击开始播放</button>');
 $('#content').html($('#content').html()+"<div style='margin-top:10px'><p style='font-weight:bold'>当前进度:&nbsp;&nbsp;<span id='progress'>0%</span></p><hr></hr><p  id='cp'>当前章节：</p><p id='np'>下一章节：</p></div>");
-$('#content').html('<hr></hr><div><p style="color:red;">3块钱一门课！全网最低！</p><p>稳定代看，包答题，包考试，95分以上</p><p id="klds"  style="color:#00BFFF;font-weight:bold;    cursor: pointer;">→ KL代刷网 ←</p></div><hr></hr>'+$('#content').html());
+
 }
 
 dragPanelMove("#skdiv","#skdiv");
