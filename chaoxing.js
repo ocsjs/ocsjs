58aca73582e6f6f4db01a6ef64b7980c59ac8162
KL-Skeleton <50533276+KL-Skeleton@users.noreply.github.com>
2020-05-03 17:02:52 +0800

    修复了章节中存在多个视频界面任务栏但是只播放一个任务栏的bug， 新增界面缩放功能，强制跳转功能。

--- chaoxing.js
+++ chaoxing.js
@@ -318,8 +318,8 @@ function check_is_done(playRate, now_video_index) {
 			mylog("当前全部视频已经播放完毕，请手动切换到要播放的视频", 'error');
 			$('#startplay').text("手动播放选择的视频");
 		} else {
-			mylog("当前章节已经播放完毕，正在切换下一章节");
-			$('#startplay').text("播放完毕，正在切换下一章节");
+			mylog("当前视频已经播放完毕，正在切换下一任务点");
+			$('#startplay').text("播放完毕，正在切换下一任务点");
 			setTimeout(clickNext, set.response_time);
 		}
 	}
