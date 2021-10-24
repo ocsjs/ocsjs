fef17338b8897f8f433e8ee9aa73bc82343db59c
enncy <50533276+enncy@users.noreply.github.com>
2021-10-24 15:18:42 +0800

    Update sk-main.js

--- cdn/chaoxing/sk-main.js
+++ cdn/chaoxing/sk-main.js
@@ -1,5 +1,50 @@
 //chatiId,unsafeWindow, $, Vue, originUrl,setting
 function  startChaoxingSK(params) {
+       // 破解倍速限制（自研）
+       let videojs = window.videojs
+       let Ext = window.Ext
+       if(videojs && Ext){
+           console.log("videojs.hook")
+           videojs.hook('beforesetup', function(videoEl, options) {
+           console.log(options)
+           options.playbackRates=[1, 1.25, 1.5, 2,4,6,8,10,12,14,16]
+           return options;
+           });
+           Ext.define("ans.VideoJs",{
+               override:"ans.VideoJs",
+               constructor: function (b) {
+                   b = b || {};
+                   var e = this;
+                   e.addEvents(["seekstart"]);
+                   e.mixins.observable.constructor.call(e, b);
+                   var c = videojs(b.videojs, e.params2VideoOpt(b.params), function () {});
+                   Ext.fly(b.videojs).on("contextmenu", function (f) {
+                       f.preventDefault();
+                   });
+                   Ext.fly(b.videojs).on("keydown", function (f) {
+                       if (f.keyCode == 32 || f.keyCode == 37 || f.keyCode == 39 || f.keyCode == 107) {
+                           f.preventDefault();
+                       }
+                   });
+                   if (c.videoJsResolutionSwitcher) {
+                       c.on("resolutionchange", function () {
+                           var g = c.currentResolution(),
+                               f = g.sources ? g.sources[0].res : false;
+                           Ext.setCookie("resolution", f);
+                       });
+                   }
+                   // 注释掉这几句代码即可破解
+                   //         var a = b.params && b.params.doublespeed ? 2 : 1;
+                   //         c.on("ratechange", function () {
+                   //             var f = c.playbackRate();
+                   //             if (f > a) {
+                   //                 c.pause();
+                   //                 c.playbackRate(1);
+                   //             }
+                   //         });
+               },
+           })
+    }
     
     let chatiId = params.chatiId
     let unsafeWindow = params.unsafeWindow
