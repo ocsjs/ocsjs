e86f216729ea4eb2b5a76fdebecd3aa84b947183
enncy <877526278@qq.com>
2021-10-31 14:32:29 +0800

    update after-pack and change latest.yml to latest.json

--- app/electron/after-pack.ts
+++ app/electron/after-pack.ts
@@ -1,18 +1,30 @@
-import { createWriteStream, existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
-import { resolve } from "path";
+import { createWriteStream, existsSync, mkdirSync, writeFileSync, readFileSync, rmSync, unlinkSync } from "fs";
+import { join, resolve } from "path";
 const yaml = require("yaml");
 const archiver = require("archiver");
 
 // 打包文件
 export default function AfterAllPack() {
-    if (!existsSync(resolve("../resource"))) {
-        mkdirSync(resolve("../resource"));
+    const root = resolve("../resource");
+    const resourcePath = resolve(join(root, "ocs-app-resource.zip"));
+    const latestPath = resolve(join(root, "latest.json"));
+    const resourceDistPath = resolve("./dist/win-unpacked/resources/app/");
+    const latestDistPath = resolve("./dist/latest.yml");
+
+    if (!existsSync(root)) {
+        mkdirSync(root);
+    } else {
+        rmSync(root, { recursive: true, force: true });
+        mkdirSync(root);
+        if (existsSync(resourcePath)) {
+            unlinkSync(resourcePath);
+        }
     }
-    const latest = readFileSync(resolve("./dist/latest.yml")).toString();
+    const latest = readFileSync(latestDistPath).toString();
     const { version, files } = yaml.parse(latest);
 
     writeFileSync(
-        resolve("../resource/latest.json"),
+        resolve(latestPath),
         JSON.stringify(
             {
                 version,
@@ -25,13 +37,13 @@ export default function AfterAllPack() {
         )
     );
 
-    var output = createWriteStream(resolve("../resource/ocs-app-resource.zip"));
+    var output = createWriteStream(resourcePath);
     var archive = archiver("zip");
 
     archive.on("error", function (err: any) {
         throw err;
     });
-    archive.directory(resolve("./dist/win-unpacked/resources/app/"), false);
+    archive.directory(resourceDistPath, false);
     archive.pipe(output);
     archive.finalize();
 
