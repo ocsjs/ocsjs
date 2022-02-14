494e523ffda8ddf5c9320be5861b42234fab4d91
enncy <877526278@qq.com>
2022-02-14 13:03:58 +0800

    feat(scripts/cx): add new login : phone-code login

--- packages/scripts/src/puppeteer/cx/login/index.ts
+++ packages/scripts/src/puppeteer/cx/login/index.ts
@@ -1,7 +1,9 @@
 import { CXPhoneLoginOptions } from "./phone";
 import { CXSchoolLoginOptions } from "./school";
 import { CXOtherLoginOptions } from "./other";
+import { CXPhoneCodeLoginOptions } from "./phone.code";
 
+export { phoneCodeLogin } from "./phone.code";
 export { phoneLogin } from "./phone";
 export { schoolLogin } from "./school";
 export { otherLogin } from "./other";
--- packages/scripts/src/puppeteer/cx/login/phone.code.ts
+++ packages/scripts/src/puppeteer/cx/login/phone.code.ts
@@ -5,7 +5,7 @@ export interface CXPhoneCodeLoginOptions {
     phone: string;
 }
 
-export async function phoneLogin(page: Page, opts: CXPhoneCodeLoginOptions) {
+export async function phoneCodeLogin(page: Page, opts: CXPhoneCodeLoginOptions) {
     const { phone } = opts;
 
     await page.goto("https://passport2.chaoxing.com/login?loginType=2&newversion=true");
