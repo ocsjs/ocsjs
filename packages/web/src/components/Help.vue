63540884ac122130945f55c8dd4ea64b2fed00f5
enncy <877526278@qq.com>
2022-03-24 08:45:57 +0800

    chore(help.vue): 更新帮助页面

--- packages/web/src/components/Help.vue
+++ packages/web/src/components/Help.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="text-start text-secondary w-100">
         <div class="m-auto help-content">
+            <div>鼠标<Key>左键</Key>点击左上角图标：</div>
+            <div><Icon title="新建文件夹" type="icon-folder-plus" /> 新建文件夹</div>
+            <div><Icon title="新建文件" type="icon-file-plus" /> 新建文件</div>
+            <div class="mb-2"><Icon title="搜索文件" type="icon-search" /> 搜索文件</div>
             <p>鼠标<Key>左键</Key>点击文件进行编辑或者运行</p>
             <p>鼠标<Key>右键</Key>点击文件显示文件菜单</p>
             <p>鼠标<Key>左键</Key>拖拽移动文件</p>
@@ -15,5 +19,9 @@ import Key from "./Key.vue";
 <style scope lang="less">
 .help-content {
     width: fit-content;
+    font-size: 0.8rem;
+}
+.ocsicon {
+    transform: translate(0.5px, -5px);
 }
 </style>
