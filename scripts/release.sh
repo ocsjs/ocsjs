c54c68385e1e500ad0434f075d2cc2907316fecd
enncy <877526278@qq.com>
2022-04-21 12:30:20 +0800

    chore: 修改 release.sh 的执行顺序

--- scripts/release.sh
+++ scripts/release.sh
@@ -18,16 +18,16 @@ if [ "$isRelease" = "y" ]; then
 
     # 代码检查
     npm run lint
-    # 更新版本
-    npm version "$version" --no-git-tag-version
     # 更新日志
     npm run changelog
+    # 更新版本
+    npm version "$version" --no-git-tag-version
     # 构建
     npm run build:core
     # 保存
     git add --all
-    git tag "$version"
     git commit -m "version release $version"
+    git tag "$version"
     # 发布
     npm publish
     echo "$version 发布成功"
