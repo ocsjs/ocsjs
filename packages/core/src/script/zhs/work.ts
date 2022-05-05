a769384feb9dd7af166ca7ced75798cc8bf5ff6e
enncy <877526278@qq.com>
2022-05-05 11:23:59 +0800

    style: 优化智慧树作业脚本的代码格式

--- packages/core/src/script/zhs/work.ts
+++ packages/core/src/script/zhs/work.ts
@@ -11,95 +11,91 @@ export async function workOrExam(setting: ScriptSettings['zhs']['work'], type: '
 
   if (setting.upload === 'close') {
     logger('warn', '自动答题已被关闭！');
-    return;
-  }
-
-  if (store.setting.answererWrappers.length === 0) {
+  } else if (store.setting.answererWrappers.length === 0) {
     logger('warn', '题库配置为空，请设置。');
-    return;
-  }
-
-  /** 清空答案 */
-  store.workResults = [];
-
-  /** 新建答题器 */
-  const worker = new OCSWorker({
-    root: '.examPaper_subject',
-    elements: {
-      title: '.subject_describe,.smallStem_describe',
-      options: '.subject_node .nodeLab'
-    },
-    /** 默认搜题方法构造器 */
-    answerer: (elements, type) =>
-      defaultAnswerWrapperHandler(store.setting.answererWrappers, type, elements.title[0].innerText),
-    work: {
-      /** 自定义处理器 */
-      handler(type, answer, option) {
-        if (type === 'judgement' || type === 'single' || type === 'multiple') {
-          if (!option.querySelector('input')?.checked) {
-            option.click();
-          }
-        } else if (type === 'completion' && answer.trim()) {
-          const text = option.querySelector('textarea');
-          if (text) {
-            text.value = answer;
-          }
-        }
-      }
-    },
-    onResult: (res) => {
-      if (res.ctx) {
-        store.workResults.push(res);
-      }
-      console.log(res);
-      logger('info', '题目完成结果 : ', res.result?.finish ? '完成' : '未完成');
-    },
-
-    /** 其余配置 */
-
-    period: (period || 3) * 1000,
-    timeout: (timeout || 30) * 1000,
-    retry,
-    stopWhenError: false
-  });
-
-  const results = await worker.doWork();
-
-  logger('info', '做题完毕', results);
-
-  if (type === 'exam') {
-    logger('info', '为了安全考虑，请自行检查后自行点击提交！');
   } else {
-    // 处理提交
-    await worker.uploadHandler({
-      uploadRate: setting.upload,
-      results,
-      async callback(finishedRate, uploadable) {
-        logger('info', '完成率 : ', finishedRate, ' , ', uploadable ? '5秒后将自动提交' : '5秒后将自动保存');
+    /** 清空答案 */
+    store.workResults = [];
 
-        await sleep(5000);
-
-        // 保存按钮， 提交按钮
-        const { saveBtn, uploadBtn } = domSearch({
-          saveBtn: '.btnStyleX:not(.btnStyleXSumit)',
-          uploadBtn: '.btnStyleXSumit'
-        });
-
-        if (uploadable) {
-          uploadBtn?.click();
-        } else {
-          saveBtn?.click();
+    /** 新建答题器 */
+    const worker = new OCSWorker({
+      root: '.examPaper_subject',
+      elements: {
+        title: '.subject_describe,.smallStem_describe',
+        options: '.subject_node .nodeLab'
+      },
+      /** 默认搜题方法构造器 */
+      answerer: (elements, type) =>
+        defaultAnswerWrapperHandler(store.setting.answererWrappers, type, elements.title[0].innerText),
+      work: {
+        /** 自定义处理器 */
+        handler(type, answer, option) {
+          if (type === 'judgement' || type === 'single' || type === 'multiple') {
+            if (!option.querySelector('input')?.checked) {
+              option.click();
+            }
+          } else if (type === 'completion' && answer.trim()) {
+            const text = option.querySelector('textarea');
+            if (text) {
+              text.value = answer;
+            }
+          }
         }
+      },
+      onResult: (res) => {
+        if (res.ctx) {
+          store.workResults.push(res);
+        }
+        console.log(res);
+        logger('info', '题目完成结果 : ', res.result?.finish ? '完成' : '未完成');
+      },
 
-        await sleep(2000);
-        /** 确定按钮 */
-        const { confirmBtn } = domSearch({
-          confirmBtn: "[role='dialog'] .el-button--primary"
-        });
+      /** 其余配置 */
 
-        confirmBtn?.click();
-      }
+      period: (period || 3) * 1000,
+      timeout: (timeout || 30) * 1000,
+      retry,
+      stopWhenError: false
     });
+
+    const results = await worker.doWork();
+
+    logger('info', '做题完毕', results);
+
+    if (type === 'exam') {
+      logger('info', '为了安全考虑，请自行检查后自行点击提交！');
+    } else {
+      // 处理提交
+      await worker.uploadHandler({
+        uploadRate: setting.upload,
+        results,
+        async callback(finishedRate, uploadable) {
+          logger('info', '完成率 : ', finishedRate, ' , ', uploadable ? '5秒后将自动提交' : '5秒后将自动保存');
+
+          await sleep(5000);
+
+          // 保存按钮， 提交按钮
+          const { saveBtn, uploadBtn } = domSearch({
+            saveBtn: '.btnStyleX:not(.btnStyleXSumit)',
+            uploadBtn: '.btnStyleXSumit'
+          });
+
+          if (uploadable) {
+            uploadBtn?.click();
+          } else {
+            saveBtn?.click();
+          }
+
+          await sleep(2000);
+          /** 确定按钮 */
+          const { confirmBtn } = domSearch({
+            confirmBtn: "[role='dialog'] .el-button--primary"
+          });
+
+          confirmBtn?.click();
+        }
+      });
+    }
   }
 }
 
@@ -109,70 +105,76 @@ export async function workOrExam(setting: ScriptSettings['zhs']['work'], type: '
 export async function creditWork(setting: ScriptSettings['zhs']['work']) {
   const { period, timeout, retry, stopWhenError } = setting;
 
-  logger('info', '即将开始做题...');
+  if (setting.upload === 'close') {
+    logger('warn', '自动答题已被关闭！');
+  } else if (store.setting.answererWrappers.length === 0) {
+    logger('warn', '题库配置为空，请设置。');
+  } else {
+    logger('info', '即将开始做题...');
 
-  const worker = new OCSWorker({
-    root: '.questionBox',
-    elements: {
-      title: '.questionContent',
-      options: '.optionUl label',
-      questionTit: '.questionTit'
-    },
-    /** 默认搜题方法构造器 */
-    answerer: (elements, type) => {
-      const title = StringUtils.nowrap(elements.title[0].innerText)
-        .trim();
-      if (title) {
-        return defaultAnswerWrapperHandler(store.setting.answererWrappers, type, title);
-      } else {
-        throw new Error('题目为空，请查看题目是否为空，或者忽略此题');
-      }
-    },
-    work: {
-      /** 自定义处理器 */
-      handler(type, answer, option, ctx) {
-        if (type === 'judgement' || type === 'single' || type === 'multiple') {
-          if (option.querySelector('input')?.checked === false) {
-            option.click();
-          }
-        } else if (type === 'completion' && answer.trim()) {
-          const text = option.querySelector('textarea');
-          if (text) {
-            text.value = answer;
+    const worker = new OCSWorker({
+      root: '.questionBox',
+      elements: {
+        title: '.questionContent',
+        options: '.optionUl label',
+        questionTit: '.questionTit'
+      },
+      /** 默认搜题方法构造器 */
+      answerer: (elements, type) => {
+        const title = StringUtils.nowrap(elements.title[0].innerText)
+          .trim();
+        if (title) {
+          return defaultAnswerWrapperHandler(store.setting.answererWrappers, type, title);
+        } else {
+          throw new Error('题目为空，请查看题目是否为空，或者忽略此题');
+        }
+      },
+      work: {
+        /** 自定义处理器 */
+        handler(type, answer, option, ctx) {
+          if (type === 'judgement' || type === 'single' || type === 'multiple') {
+            if (option.querySelector('input')?.checked === false) {
+              option.click();
+            }
+          } else if (type === 'completion' && answer.trim()) {
+            const text = option.querySelector('textarea');
+            if (text) {
+              text.value = answer;
+            }
           }
         }
-      }
-    },
-    onResult: (res) => {
-      if (res.ctx) {
-        // 浅拷贝并且只保留 innerText ， 防止元素对象变化导致显示的题目全部是一个同样的值。
+      },
+      onResult: (res) => {
+        if (res.ctx) {
+          // 浅拷贝并且只保留 innerText ， 防止元素对象变化导致显示的题目全部是一个同样的值。
 
-        if (res.ctx.elements.title[0]) {
-          // @ts-ignore
-          res.ctx.elements.title[0] = {
-            innerText: res.ctx.elements.questionTit[0]?.innerText + res.ctx.elements.title[0]?.innerText
-          };
+          if (res.ctx.elements.title[0]) {
+            // @ts-ignore
+            res.ctx.elements.title[0] = {
+              innerText: res.ctx.elements.questionTit[0]?.innerText + res.ctx.elements.title[0]?.innerText
+            };
+          }
+
+          store.workResults.push(res);
         }
+        console.log(res);
+        logger('info', '题目完成结果 : ', res.result?.finish ? '完成' : '未完成');
+      },
+      period: (period || 3) * 1000,
+      timeout: (timeout || 30) * 1000,
+      retry,
+      stopWhenError
+    });
 
-        store.workResults.push(res);
-      }
-      console.log(res);
-      logger('info', '题目完成结果 : ', res.result?.finish ? '完成' : '未完成');
-    },
-    period: (period || 3) * 1000,
-    timeout: (timeout || 30) * 1000,
-    retry,
-    stopWhenError
-  });
+    const getBtn = () => document.querySelector('span.Topicswitchingbtn:nth-child(2)') as HTMLElement;
+    let next = getBtn();
 
-  const getBtn = () => document.querySelector('span.Topicswitchingbtn:nth-child(2)') as HTMLElement;
-  let next = getBtn();
-
-  while (next) {
-    await worker.doWork();
-    await sleep((period || 3) * 1000);
-    next = getBtn();
-    next?.click();
-    await sleep((period || 3) * 1000);
+    while (next) {
+      await worker.doWork();
+      await sleep((period || 3) * 1000);
+      next = getBtn();
+      next?.click();
+      await sleep((period || 3) * 1000);
+    }
   }
 }
