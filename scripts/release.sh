a855740f0b8ffa107f5f580f2123c70baedb16f7
enncy <877526278@qq.com>
2022-05-24 21:16:58 +0800

    fix(root): 优化构建发布release.sh文件

--- scripts/release.sh
+++ scripts/release.sh
@@ -30,6 +30,8 @@ if [ "$isRelease" = "y" ]; then
     git tag "$version" &&
     # 发布
     npm publish
+    # 提交
+    git push origin 3.0
     echo "$version 发布成功"
     elif [ "$isRelease" = "n" ]; then
     echo "取消发布"
