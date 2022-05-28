a6a2941c059510b47b34deba4f7b925c8644e6f2
enncy <877526278@qq.com>
2022-05-28 18:05:52 +0800

    chore(root): 修改热更新命令

--- scripts/dev-core.js
+++ scripts/dev-core.js
@@ -2,5 +2,5 @@ const { series } = require('gulp');
 const { execOut } = require('./utils');
 
 exports.default = series(
-  () => execOut('vite build -w --clearScreen --emptyOutDir false', { cwd: '../packages/core' })
+  () => execOut('vite build -w --emptyOutDir false', { cwd: '../packages/core' })
 );
