837ed30cc361a1e9536d812d53c2d488bb4d806d
enncy <877526278@qq.com>
2022-05-31 00:04:27 +0800

    chore(core): 国开脚本预留

---
+++ packages/core/src/script/ouchn/index.ts
@@ -0,0 +1,23 @@
+
+import { createNote } from '../../components';
+import { defineScript } from '../../core/define.script';
+
+export const OUCHNScript = defineScript({
+  name: '国家开放大学',
+  routes: [
+
+  ],
+  panels: [
+    {
+      name: '国开小助手',
+      url: '**ouchn.cn/**Login**',
+      el: () => createNote('提示您:', '登录后才能使用脚本功能哦')
+    },
+    {
+      name: '国开小助手',
+      url: '**ouchn.cn/site/ouchnPc/index**',
+      el: () => createNote('提示您:', '请点击任意的课程进入。')
+    }
+
+  ]
+});
