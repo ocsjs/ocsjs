7ad7577fd0a8da716d5ae86a57fb61c07bca1fa9
enncy <877526278@qq.com>
2022-06-04 21:31:19 +0800

    build(root): 更新 pnpm build 命令内容，全局运行 tsc 指令

--- scripts/tsc.js
+++ scripts/tsc.js
@@ -2,7 +2,7 @@ const { series } = require('gulp');
 const del = require('del');
 const { execOut } = require('./utils');
 
-function cleanOutput () {
+function cleanOutput() {
   return del(['../packages/common/lib', '../packages/core/lib', '../packages/scripts/lib'], { force: true });
 }
 
@@ -10,7 +10,9 @@ exports.default = series(
   cleanOutput,
   series(
     () => execOut('tsc', { cwd: '../packages/common' }),
+    () => execOut('tsc', { cwd: '../packages/scripts' }),
+    () => execOut('tsc', { cwd: '../packages/app' }),
     () => execOut('tsc', { cwd: '../packages/core' }),
-    () => execOut('tsc', { cwd: '../packages/scripts' })
+    () => execOut('tsc', { cwd: '../packages/web' })
   )
 );
