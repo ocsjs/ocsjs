be099b07eecb500b67adb6c2b8f6f4accb63d445
enncy <877526278@qq.com>
2022-06-04 21:40:57 +0800

    style(app): 修改软件路由模式

--- packages/web/src/route/index.ts
+++ packages/web/src/route/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { config } from '../config';
 
 /**
@@ -8,7 +8,7 @@ import { config } from '../config';
 export const routes: RouteRecordRaw[] = Reflect.ownKeys(config.routes).map((key) => Reflect.get(config.routes, key));
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
