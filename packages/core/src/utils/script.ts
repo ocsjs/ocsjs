cc5b7d15b24825afcb11a7767a3157de4ce88c3a
enncy <877526278@qq.com>
2022-12-29 02:21:40 +0800

    style(core): 修改 $createSelectOptions 参数

--- packages/core/src/utils/script.ts
+++ packages/core/src/utils/script.ts
@@ -7,12 +7,15 @@ import { el } from './dom';
 /**
  * 创建 select 元素的子选项
  * @param selectedValue
- * @param options [value,text]
+ * @param options [value,text,title]
  * @returns
  */
-export function $createSelectOptions(selectedValue: string | null = '', options: [any, string][]) {
+export function $createSelectOptions(
+	selectedValue: string | null = '',
+	options: ([any, string] | [any, string, string])[]
+) {
 	return options.map((opt) =>
-		el('option', { value: String(opt[0]), innerText: opt[1] }, (opt) => {
+		el('option', { value: String(opt[0]), innerText: opt[1], title: opt[2] }, (opt) => {
 			if (opt.value === selectedValue) {
 				opt.toggleAttribute('selected');
 			}
