7492e75e5ca6ce907407c2745435ab2c2ff68dd4
enncy <877526278@qq.com>
2023-03-11 14:52:08 +0800

    style(script): 将所有答题设置移动到全局设置

--- packages/scripts/src/projects/common.ts
+++ packages/scripts/src/projects/common.ts
@@ -9,18 +9,238 @@ import {
 	$store,
 	Project,
 	Script,
+	request,
 	$creator,
-	SimplifyWorkResult
+	SimplifyWorkResult,
+	$script
 } from '@ocsjs/core';
 
 import type { ScriptPanelElement, HeaderElement, AnswererWrapper } from '@ocsjs/core';
 import { definedProjects } from '../index';
 import { $console } from './background';
+import { markdown } from '../utils/markdown';
+import { workConfigs } from '../utils/configs';
 
 export const CommonProject = Project.create({
 	name: '通用',
 	domains: [],
+	level: 1,
 	scripts: {
+		guide: new Script({
+			name: '使用教程',
+			url: [['所有页面', /.*/]],
+			namespace: 'common.guide',
+			onrender({ panel, header }) {
+				const guide = createGuide({ panel, header });
+
+				const home = el('button', { className: 'base-style-button-secondary' }, '🏡官网教程');
+				home.onclick = () => window.open('https://docs.ocsjs.com', '_blank');
+
+				const notify = el('button', { className: 'base-style-button-secondary' }, '✨查看通知提示');
+				notify.onclick = () => $script.pin(CommonProject.scripts.notify);
+
+				const changeLog = el('button', { className: 'base-style-button-secondary' }, '📄查看更新日志');
+				changeLog.onclick = () => $script.pin(CommonProject.scripts.changelog);
+
+				changeLog.style.marginBottom = '12px';
+				guide.style.width = '400px';
+				panel.body.replaceChildren(home, notify, changeLog, guide);
+			}
+		}),
+		notify: new Script({
+			name: '通知提示',
+			url: [['', /.*/]],
+			namespace: 'common.notify',
+			configs: {
+				notes: {
+					defaultValue: $creator.notes([
+						'此页面实时更新，大家遇到问题可以看看通知',
+						'或者进入上方官网里的交流群进行反馈'
+					]).outerHTML
+				}
+			},
+			async onrender({ panel }) {
+				panel.body.replaceChildren('加载中...');
+				const md = await request('https://cdn.ocsjs.com/articles/ocs/notify.md', {
+					type: 'fetch',
+					contentType: 'text',
+					method: 'get'
+				});
+				panel.body.replaceChildren(el('div', { className: 'card markdown', innerHTML: markdown(md) }));
+			}
+		}),
+		settings: new Script({
+			name: '全局设置',
+			url: [['所有页面', /.*/]],
+			namespace: 'common.settings',
+			configs: {
+				notes: {
+					defaultValue: $creator.notes([
+						'✨鼠标移动到按钮或者输入框，可以看到提示！',
+						'想要自动答题必须设置 “题库配置” ',
+						'设置后进入章节测试，作业，考试页面即可自动答题。'
+					]).outerHTML
+				},
+				notification: {
+					label: '开启系统通知',
+					defaultValue: true,
+					attrs: {
+						title:
+							'允许脚本发送系统通知，只有重要事情发生时会发送系统通知，尽量避免用户受到骚扰（在电脑屏幕右侧显示通知弹窗，例如脚本执行完毕，版本更新等通知）。',
+						type: 'checkbox'
+					}
+				},
+				answererWrappers: {
+					defaultValue: [] as AnswererWrapper[]
+				},
+
+				answererWrappersButton: {
+					label: '题库配置',
+					defaultValue: '点击配置',
+					attrs: {
+						type: 'button'
+					},
+					onload() {
+						const aws: any[] = CommonProject.scripts.settings.cfg.answererWrappers || [];
+						this.value = aws.length ? '当前有' + aws.length + '个可用题库' : '点击配置';
+
+						this.onclick = () => {
+							const aw: any[] = CommonProject.scripts.settings.cfg.answererWrappers || [];
+							const copy = $creator.copy('复制题库配置', JSON.stringify(aw));
+
+							const list = el('div', [
+								el('div', aw.length ? ['以下是已经解析过的题库配置：', copy] : ''),
+								...createAnswererWrapperList(aw)
+							]);
+
+							const model = $model('prompt', {
+								content: el('div', [
+									'具体配置教程，请查看官网：',
+									el('a', { href: 'https://docs.ocsjs.com/docs/work' }, '自动答题教程'),
+									list
+								]),
+								placeholder: aw.length ? '重新输入' : '输入题库配置',
+								cancelButton: el('button', {
+									className: 'model-cancel-button',
+									innerText: '清空题库配置',
+									onclick() {
+										$message('success', { content: '已清空，在答题前请记得重新配置。' });
+										model?.remove();
+										CommonProject.scripts.settings.cfg.answererWrappers = [];
+									}
+								}),
+								async onConfirm(value) {
+									if (value) {
+										try {
+											const aw = await AnswerWrapperParser.from(value);
+											if (aw.length) {
+												CommonProject.scripts.settings.cfg.answererWrappers = aw;
+
+												$model('alert', {
+													content: el('div', [
+														el('div', '配置成功，打开具有答题脚本的页面后即可自动答题，解析到的题库如下所示:'),
+														...createAnswererWrapperList(aw)
+													])
+												});
+											} else {
+												$model('alert', { content: '题库配置不能为空，请重新配置。' });
+											}
+										} catch (e: any) {
+											$model('alert', {
+												content: el('div', [el('div', '解析失败，原因如下 :'), el('div', e.message)])
+											});
+										}
+									} else {
+										$model('alert', {
+											content: el('div', '不能为空！')
+										});
+									}
+								}
+							});
+						};
+					}
+				},
+				upload: workConfigs.upload,
+				skipAnswered: {
+					label: '跳过已经完成的题目',
+					attrs: { type: 'checkbox', title: '当题目中的选项大于一个已经被选中，则将跳过此题的答题。' },
+					defaultValue: true
+				},
+				uncheckAllChoice: {
+					label: '清空答案',
+					attrs: { type: 'checkbox', title: '在考试开始前，清空所有已经选择过的答案。' },
+					defaultValue: false
+				},
+				thread: {
+					label: '线程数量（个）',
+					attrs: {
+						type: 'number',
+						min: 1,
+						step: 1,
+						max: 3,
+						title:
+							'同一时间内答题线程工作的数量（例子：三个线程则代表一秒内同时搜索三道题），过多可能导致题库服务器压力过大，请适当调低。'
+					},
+					defaultValue: 1
+				},
+				period: {
+					label: '答题间隔（秒）',
+					attrs: {
+						type: 'number',
+						min: 0,
+						step: 1,
+						max: 60,
+						title: '每道题的间隔时间，不建议太低，避免增加服务器压力。'
+					},
+					defaultValue: 3
+				},
+				timeout: {
+					label: '处理超时时间（秒）',
+					attrs: { type: 'number', min: 3, step: 1, max: 60, title: '每道题最多做n秒, 超过则跳过此题。' },
+					defaultValue: 30
+				},
+				retry: {
+					label: '超时重试次数',
+					attrs: { type: 'number', min: 0, step: 1, max: 3 },
+					defaultValue: 1
+				},
+				forceWork: {
+					label: '(仅超星)强制答题',
+					defaultValue: false,
+					attrs: {
+						type: 'checkbox',
+						title:
+							'当章节测试不是任务点时，强制自动答题。\n(左上角有黄点的代表此小节是任务点)\n(一般来说不是任务点的章节测试是不计分的)'
+					}
+				},
+				'randomWork-choice': {
+					defaultValue: false,
+					label: '(仅超星)随机选择',
+					attrs: { type: 'checkbox', title: '随机选择任意一个选项' }
+				},
+				'randomWork-complete': {
+					defaultValue: false,
+					label: '(仅超星)随机填空',
+					attrs: { type: 'checkbox', title: '随机填写以下任意一个文案' }
+				},
+				'randomWork-completeTexts-textarea': {
+					defaultValue: ['不会', '不知道', '不清楚', '不懂', '不会写'].join('\n'),
+					label: '(仅超星)随机填空文案',
+					tag: 'textarea',
+					attrs: { title: '每行一个，随机填入' }
+				}
+			},
+			oncomplete() {
+				this.onConfigChange('notification', (open) => {
+					if (open) {
+						$gm.notification('您已开启系统通知，如果脚本有重要情况需要处理，则会发起通知提示您。', {
+							duration: 5
+						});
+					}
+				});
+			},
+			onbeforeunload() {}
+		}),
 		onlineSearch: new Script({
 			name: '在线搜题',
 			url: [['所有页面', /.*/]],
@@ -120,9 +340,9 @@ export const CommonProject = Project.create({
 			configs: {
 				notes: {
 					defaultValue: $creator.notes([
+						['警告：禁止与其他脚本一起使用，', '否则出现答案选不上或者其他问题一律后果自负。'],
 						'点击题目序号，查看搜索结果',
-						'每次自动答题开始前，都会清空上一次的搜索结果。',
-						'点击下列题目或者序号进行固定显示。'
+						'每次自动答题开始前，都会清空上一次的搜索结果。'
 					]).outerHTML
 				},
 				/**
@@ -406,171 +626,23 @@ export const CommonProject = Project.create({
 				$store.addChangeListener('common.work-results.result', debounce(render, 1000, { maxWait: 1000 }));
 			}
 		}),
-		settings: new Script({
-			name: '全局设置',
-			url: [['所有页面', /.*/]],
-			namespace: 'common.settings',
+		changelog: new Script({
+			name: '版本更新',
+			url: [['', /.*/]],
+			namespace: 'common.changelog',
 			configs: {
-				notification: {
-					label: '开启系统通知',
-					defaultValue: true,
-					attrs: {
-						title:
-							'允许脚本发送系统通知，只有重要事情发生时会发送系统通知，尽量避免用户受到骚扰（在电脑屏幕右侧显示通知弹窗，例如脚本执行完毕，版本更新等通知）。',
-						type: 'checkbox'
-					}
-				},
-				answererWrappers: {
-					defaultValue: [] as AnswererWrapper[]
-				},
-
-				answererWrappersButton: {
-					label: '题库配置',
-					defaultValue: '点击配置',
-					attrs: {
-						type: 'button'
-					},
-					onload() {
-						const aws: any[] = CommonProject.scripts.settings.cfg.answererWrappers || [];
-						this.value = `点击重新配置${aws.length ? '，当前有' + aws.length + '个可用题库' : ''}`;
-
-						this.onclick = () => {
-							const aw: any[] = CommonProject.scripts.settings.cfg.answererWrappers || [];
-							const copy = $creator.copy('复制题库配置', JSON.stringify(aw));
-
-							const list = el('div', [
-								el('div', aw.length ? ['以下是已经解析过的题库配置：', copy] : ''),
-								...createAnswererWrapperList(aw)
-							]);
-
-							const model = $model('prompt', {
-								content: el('div', [
-									'具体配置教程，请查看官网：',
-									el('a', { href: 'https://docs.ocsjs.com/docs/work' }, '自动答题教程'),
-									list
-								]),
-								placeholder: aw.length ? '重新输入' : '输入题库配置',
-								cancelButton: el('button', {
-									className: 'model-cancel-button',
-									innerText: '清空题库配置',
-									onclick() {
-										$message('success', { content: '已清空，在答题前请记得重新配置。' });
-										model?.remove();
-										CommonProject.scripts.settings.cfg.answererWrappers = [];
-									}
-								}),
-								async onConfirm(value) {
-									if (value) {
-										try {
-											const aw = await AnswerWrapperParser.from(value);
-											CommonProject.scripts.settings.cfg.answererWrappers = aw;
-
-											$model('alert', {
-												content: el('div', [
-													el('div', '配置成功，打开具有答题脚本的页面后即可自动答题，解析到的题库如下所示:'),
-													...createAnswererWrapperList(aw)
-												])
-											});
-										} catch (e: any) {
-											$model('alert', {
-												content: el('div', [el('div', '解析失败，原因如下 :'), el('div', e.message)])
-											});
-										}
-									} else {
-										$model('alert', {
-											content: el('div', '不能为空！')
-										});
-									}
-								}
-							});
-						};
-					}
-				},
-				skipAnswered: {
-					label: '跳过已经完成的题目',
-					attrs: { type: 'checkbox', title: '当题目中的选项大于一个已经被选中，则将跳过此题的答题。' },
-					defaultValue: true
-				},
-				uncheckAllChoice: {
-					label: '清空答案',
-					attrs: { type: 'checkbox', title: '在考试开始前，清空所有已经选择过的答案。' },
-					defaultValue: false
-				},
-				thread: {
-					label: '线程数量（个）',
-					attrs: {
-						type: 'number',
-						min: '1',
-						step: '1',
-						max: '3',
-						title: '同一时间内答题线程工作的数量，过多可能导致题库服务器压力过大，请适当调低。'
-					},
-					defaultValue: 1
-				},
-				period: {
-					label: '答题间隔（秒）',
-					attrs: {
-						type: 'number',
-						min: '0',
-						step: '1',
-						max: '60',
-						title: '每道题的间隔时间，不建议太低，避免增加服务器压力。'
-					},
-					defaultValue: 3
-				},
-				timeout: {
-					label: '处理超时时间（秒）',
-					attrs: { type: 'number', min: '10', step: '1', max: '60', title: '每道题最多做n秒, 超过则跳过此题。' },
-					defaultValue: 30
-				},
-				retry: {
-					label: '超时重试次数',
-					attrs: { type: 'number', min: '0', step: '1', max: '3' },
-					defaultValue: 1
+				notes: {
+					defaultValue: $creator.notes(['此页面实时更新，遇到问题可以查看最新版本是否修复。']).outerHTML
 				}
 			},
-			oncomplete() {
-				this.onConfigChange('notification', (open) => {
-					if (open) {
-						$gm.notification('您已开启系统通知，如果脚本有重要情况需要处理，则会发起通知提示您。', {
-							duration: 5
-						});
-					}
+			async onrender({ panel }) {
+				panel.body.replaceChildren('加载中...');
+				const md = await request('https://cdn.ocsjs.com/articles/ocs/changelog.md', {
+					type: 'fetch',
+					contentType: 'text',
+					method: 'get'
 				});
-			},
-			onbeforeunload() {}
-		}),
-		guide: new Script({
-			name: '使用教程',
-			url: [
-				['智慧树首页', 'https://www.zhihuishu.com/'],
-				['智慧树个人中心', 'https://onlineweb.zhihuishu.com/onlinestuh5'],
-				['超星首页', 'https://www.chaoxing.com/'],
-				['超星个人中心', 'http://i.chaoxing.com/']
-			],
-			namespace: 'common.guide',
-			configs: {
-				showGuide: { defaultValue: true, label: '进入页面，显示使用教程', attrs: { type: 'checkbox' } }
-			},
-			onrender({ panel, header }) {
-				const guide = createGuide({ panel, header });
-				guide.style.width = '460px';
-				panel.body.replaceChildren(el('hr'), guide);
-			},
-			onhistorychange() {
-				this.oncomplete?.();
-			},
-			oncomplete() {
-				if (this.cfg.showGuide && this.panel && this.header) {
-					$model('confirm', {
-						title: '使用教程',
-						content: createGuide({ panel: this.panel, header: this.header }),
-						confirmButtonText: '我已阅读，不再提示',
-						onConfirm: () => {
-							this.cfg.showGuide = false;
-						}
-					});
-				}
+				panel.body.replaceChildren(el('div', { className: 'markdown card', innerHTML: markdown(md) }));
 			}
 		}),
 		hack: new Script({
@@ -645,91 +717,68 @@ function createAnswererWrapperList(aw: AnswererWrapper[]) {
 }
 
 const createGuide = ({ panel, header }: { panel: ScriptPanelElement; header: HeaderElement }) => {
-	const projectSelector = header.projectSelector?.cloneNode(true) as HTMLSelectElement;
-	const logo = header.logo?.cloneNode(true) as HTMLDivElement;
-	const expandSwitcher = header.expandSwitcher?.cloneNode(true) as HTMLDivElement;
-	const visualSwitcher = header.visualSwitcher?.cloneNode(true) as HTMLDivElement;
-	const closeButton = header.closeButton?.cloneNode(true) as HTMLDivElement;
-
-	if (projectSelector.style) {
-		projectSelector.style.display = 'inline-block';
-		projectSelector.querySelectorAll('select').forEach((el) => (el.disabled = true));
-		logo.style.cursor = 'default';
-		expandSwitcher.style.cursor = 'default';
-		visualSwitcher.style.cursor = 'default';
-		closeButton.style.cursor = 'default';
-	}
-
 	return el('div', { className: 'user-guide card' }, [
 		$creator.notes(
 			[
+				'打开任意网课平台，等待脚本加载，',
+				'脚本加载后查看每个网课不同的使用提示。',
+				'如果不支持当前网课，则不会有相应的提示以及设置面板。',
 				[
-					'OCS会根据当前的页面自动选择脚本进行运行，并出现使用提示。',
-					'如果没有达到您预期的效果，则代表当前页面并没有脚本运行。',
 					'以下是全部支持的网课以及包含的脚本（点击下列详情展开查看）:',
-					...definedProjects().map((project) => {
-						return el('details', [
-							el('summary', project.name),
-							el(
-								'ul',
-								Object.keys(project.scripts).map((key) => {
-									const script = project.scripts[key];
+					...definedProjects()
+						.filter((p) => p.studyProject)
+						.map((project) => {
+							return el('details', [
+								el('summary', project.name),
+								el(
+									'ul',
+									Object.keys(project.scripts).map((key) => {
+										const script = project.scripts[key];
 
-									return el(
-										'li',
-										[
-											el('b', script.name),
-											$creator.notes([
-												el('span', ['操作面板：', script.hideInPanel ? '隐藏' : '显示']),
+										return el(
+											'li',
+											[
+												el('b', script.name),
+												$creator.notes([
+													el('span', ['操作面板：', script.hideInPanel ? '隐藏' : '显示']),
 
-												[
-													'运行页面：',
-													el(
-														'ul',
-														script.url.map((i) =>
-															el('li', [
-																i[0],
-																'：',
-																i[1] instanceof RegExp
-																	? i[1].toString().replace(/\\/g, '').slice(1, -1)
-																	: el('a', { href: i[1], target: '_blank' }, i[1])
-															])
+													[
+														'运行页面：',
+														el(
+															'ul',
+															script.url.map((i) =>
+																el('li', [
+																	i[0],
+																	'：',
+																	i[1] instanceof RegExp
+																		? i[1].toString().replace(/\\/g, '').slice(1, -1)
+																		: el('a', { href: i[1], target: '_blank' }, i[1])
+																])
+															)
 														)
-													)
-												]
-											])
-										],
-										(li) => {
-											li.style.marginBottom = '12px';
-										}
-									);
-								}),
-								(ul) => {
-									ul.style.paddingLeft = '42px';
-								}
-							)
-						]);
-					}),
+													]
+												])
+											],
+											(li) => {
+												li.style.marginBottom = '12px';
+											}
+										);
+									}),
+									(ul) => {
+										ul.style.paddingLeft = '42px';
+									}
+								)
+							]);
+						}),
 					el('br')
 				],
 
 				[
-					'以下是窗口顶部菜单栏的按钮说明:',
-					$creator.notes([
-						el('span', [
-							projectSelector,
-							' 部分脚本会提供操作页面（包含脚本设置和脚本提示）。',
-							el('div', ['此按钮可以选择指定的脚本页面进行显示。'])
-						]),
-						el('span', [logo, ' 点击查看官网教程']),
-						el('span', [expandSwitcher, ' 可以 展开/收缩 脚本操作页面。']),
-						el('span', [visualSwitcher, ' 可以 展开/收缩 窗口。']),
-						el('span', [closeButton, ' ', closeButton.getAttribute('data-title') || ''])
-					]),
-					el('br')
-				],
-
-				['最后温馨提示：请将浏览器页面保持最大化，或者缩小窗口，不能最小化，', '否则可能导致脚本卡死！']
+					'最后温馨提示: ',
+					'- 禁止与其他脚本一起使用，否则出现答案选不上或者页面卡死，无限刷新，等问题一律后果自负。',
+					'- 任何其他疑问请前往官网查看交流群，进入交流群后带截图进行反馈。',
+					'- 请将浏览器页面保持最大化，或者缩小窗口，不能最小化，否则可能导致脚本卡死！'
+				]
 			],
 			'ol'
 		)
